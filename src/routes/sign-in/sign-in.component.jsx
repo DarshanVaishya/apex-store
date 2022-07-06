fd3a8eb06ba1e9d2e12f4595dafb6069846eb0e8
Darshan Vaishya <DarshanVaishya3@gmail.com>
2022-07-06 19:37:05 +0530

    Feat:  Google auth token  stored in firestore db

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,10 +1,15 @@
 import React from "react";
-import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils";
+import {
+	createUserDocumentFromAuth,
+	signInWithGooglePopup,
+} from "../../utils/firebase/firebase.utils";
 
 function SignIn() {
 	const logGoogleUser = async () => {
 		const response = await signInWithGooglePopup();
-		console.log(response);
+		console.log("RESPONSE", response);
+
+		createUserDocumentFromAuth(response.user);
 	};
 
 	return (
