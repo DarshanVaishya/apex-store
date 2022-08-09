5d4821f8308c232c525a562ce740a8e9e510775e
Darshan Vaishya <DarshanVaishya3@gmail.com>
2022-08-09 18:25:26 +0530

    Feat: Added Error404 to dynamic links

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,10 +1,12 @@
 import { useEffect } from "react";
+import { useRef } from "react";
 import { useState } from "react";
 import { useContext } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import Button from "../../components/button/button.component";
 import ProductCard from "../../components/product-card/product-card.component";
 import { categoriesContext } from "../../contexts/categories.context";
+import Error404 from "../Error404/Error404.component";
 
 import styles from "./category.module.scss";
 
@@ -13,13 +15,18 @@ function Category() {
 	const { categoriesMap } = useContext(categoriesContext);
 	const [products, setProducts] = useState([]);
 	const navigate = useNavigate();
+	const correctCategory = useRef(true);
 	const clickHandler = () => navigate("/shop");
 
 	useEffect(() => {
-		setProducts(categoriesMap[category]);
+		setProducts(categoriesMap[category] || []);
+		const categories = Object.keys(categoriesMap);
+		if (categories.length === 0) return;
+		correctCategory.current = categories.some((cat) => cat === category);
 	}, [category, categoriesMap]);
 
-	if (!products) return;
+	if (!correctCategory.current) return <Error404 />;
+	if (products.length === 0) return <h1>Loading...</h1>;
 	return (
 		<div className="padding-container">
 			<h2 className={styles.header}>
