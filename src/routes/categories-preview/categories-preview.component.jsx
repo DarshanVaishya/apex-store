2302472d7f93e8c136d62bafd5fc46c93c9c98bc
Darshan Vaishya <DarshanVaishya3@gmail.com>
2022-08-09 19:00:34 +0530

    Feat: added loader to categories preview

--- src/routes/categories-preview/categories-preview.component.jsx
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -1,13 +1,17 @@
 import { useContext } from "react";
 import CategoryPreview from "../../components/category-preview/category-preview.component";
 import { categoriesContext } from "../../contexts/categories.context";
+import Loader from "../../components/loader/loader.component";
 
 function CategoriesPreview() {
 	const { categoriesMap } = useContext(categoriesContext);
+	const categories = Object.keys(categoriesMap);
 
+	if (categories.length === 0) return <Loader />;
 	return (
 		<>
-			{Object.keys(categoriesMap).map((title) => (
+			<Loader />
+			{categories.map((title) => (
 				<CategoryPreview
 					key={title}
 					title={title}
